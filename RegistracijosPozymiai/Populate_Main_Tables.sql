67002dfd4bef81dcf345712eb53bc413e880b0a7
Ernestekas <ernestekas@gmail.com>
2022-01-04 17:26:45 +0200

    Populate DB main tables sql file added.

---
+++ RegistracijosPozymiai/Populate_Main_Tables.sql
@@ -0,0 +1,22 @@
+﻿INSERT INTO RegAttributes (Name)
+VALUES
+('Reikia atlikti rangos darbus'),
+('Rangos darbus atliks'),
+('Verslo klientas'),
+('Skaičiavimo metodas'),
+('Svarbus klientas'),
+('Klientas yra bumbuliukas');
+
+INSERT INTO RegValues (Name, RegistrationAttributeId)
+VALUES
+('Taip', 1),
+('Ne', 1),
+('Metinis rangovas', 2),
+('Ramūnas iš Zabludiškių', 2),
+('Naujai samdyti rangovai', 2),
+('Taip', 3),
+('Ne', 3),
+('Automatinis', 4),
+('Rankinis', 4),
+('Taip', 6),
+('Ne', 6);
